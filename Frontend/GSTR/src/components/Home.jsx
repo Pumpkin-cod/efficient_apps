3e15e11597594d6e46e638ec68c45070034238e2
Catherine Okang-Boye <katieboye44@gmail.com>
2023-05-25 09:23:05 +0000

    hero page in progress

--- Frontend/GSTR/src/components/Home.jsx
+++ Frontend/GSTR/src/components/Home.jsx
@@ -1,9 +1,12 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import HeroSection from './HeroSection'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <HeroSection />
+    </div>
   )
 }
 
--- Frontend/GSTR/src/components/Signup.jsx
+++ Frontend/GSTR/src/components/Signup.jsx
@@ -1,10 +1,60 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import imageSrc from '../images/Allura - Feedback Session.png';
 
-const Signup = () => {
+function SignUp() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div>Signup</div>
-  )
+    <div className='big-container'>
+      <div className='container'>
+        <h2>Sign Up</h2>
+        <form className='login-form' onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <label htmlFor='name'>Name</label>
+            <input type='text' id='name' {...register('name', { required: true })} />
+            {errors.name && <span className='error'>Name is required</span>}
+          </div>
+          <div>
+            <label htmlFor='email'>Email</label>
+            <input type='email' id='email' {...register('email', { required: true, pattern: /^\S+@\S+$/i })} />
+            {errors.email && <span className='error'>Invalid email address</span>}
+          </div>
+          <div>
+            <label htmlFor='password'>Password</label>
+            <input type='password' id='password' {...register('password', { required: true, minLength: 6 })} />
+            {errors.password && <span className='error'>Password must be at least 6 characters</span>}
+          </div>
+          <div>
+            <label htmlFor='confirmPassword'>Confirm Password</label>
+            <input type='password' id='confirmPassword' {...register('confirmPassword', { required: true, minLength: 6 })} />
+            {errors.confirmPassword && <span className='error'>Password confirmation is required</span>}
+          </div>
+          <button type='submit'>Sign Up</button>
+        </form>
+        <div className='additional-options'>
+          <div className='remember-me'>
+            <input type='checkbox' id='rememberMe' {...register('rememberMe')} />
+            <label htmlFor='rememberMe'>Remember Me</label>
+          </div>
+          <div className='forgot-password'>
+            <a href='#'>Forgot Password?</a>
+          </div>
+        </div>
+        <div className='signup-message'>
+          Already have an account? <a href='/login'>Login</a>
+        </div>
+      </div>
+      <div className='img-container'>
+        <img className='img-blush' src={imageSrc} alt='Image' />
+      </div>
+    </div>
+  );
 }
 
-export default Signup
+export default SignUp;
